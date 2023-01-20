e0f47064eab5c3e6bd7c77194fcda73854309294
GustavJohansson420 <gujo04002@utb.vaxjo.se>
2023-01-20 09:25:30 +0100

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Uppgift_2._1
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vilken stad kommer du ifrån?");
+            string strNr1 = Console.ReadLine();
+            Console.WriteLine("Vart är din drömresa?");
+            string strNr2 = Console.ReadLine();
+            Console.WriteLine("Hej " + strNr1 + "s bo. " + "Jag hoppas du får resa till " + strNr2 + ". :)");
+        }
+    }
+}
